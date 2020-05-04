3d4f8bb4046caf216d2ab8fe5e2b333a4905b191
KicaRonaldOkello <okello.ronald045@gmail.com>
2020-05-05 00:22:53 +0300

    Add social sharing

--- src/app/modules/blog/display-single-blog/display-single-blog.component.ts
+++ src/app/modules/blog/display-single-blog/display-single-blog.component.ts
@@ -6,6 +6,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { AuthService } from 'src/app/services/guards/auth.service';
 import Helpers from 'src/app/helpers/helpers';
 import { ShareDataService } from 'src/app/services/share-data/share-data.service';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-display-single-blog',
@@ -20,21 +21,47 @@ export class DisplaySingleBlogComponent implements OnInit {
   commentPosted = false;
   articleDoesNotExist = false;
   errorMessage = '';
+  currentUrl;
+  encodedUrl;
+  weiboUrl;
+  displayShare = false;
   constructor(
     private blogService: GetBlogsService,
     private activatedRoute: ActivatedRoute,
     private sanitizer: DomSanitizer,
     private router: Router,
     private auth: AuthService,
+    private matIconRegistry: MatIconRegistry,
     private sharedService: ShareDataService,
     private commentService: CommentsService) {
       this.sharedService.showAd('true');
+      this.matIconRegistry.addSvgIcon(
+        "facebook",
+        this.sanitizer.bypassSecurityTrustResourceUrl("../assets/facebook.svg")
+      );
+      this.matIconRegistry.addSvgIcon(
+        "twitter",
+        this.sanitizer.bypassSecurityTrustResourceUrl("../assets/tweet.svg")
+      );
+      this.matIconRegistry.addSvgIcon(
+        "whatsapp",
+        this.sanitizer.bypassSecurityTrustResourceUrl("../assets/whatsapp.svg")
+      );
+      this.matIconRegistry.addSvgIcon(
+        "wechat",
+        this.sanitizer.bypassSecurityTrustResourceUrl("../assets/wechat.svg")
+      );
+      this.matIconRegistry.addSvgIcon(
+        "weibo",
+        this.sanitizer.bypassSecurityTrustResourceUrl("../assets/weibo.svg")
+      );
     }
 
   ngOnInit() {
     this.blogService.getBlogs(1, 2).subscribe(blog => {
       this.blogs = blog.articles;
     });
+    // this.loadDynmicallyScript();
 
       this.id = this.activatedRoute.snapshot.paramMap.get('id');
       this.id = this.id.replace('_', '/');
@@ -47,8 +74,12 @@ export class DisplaySingleBlogComponent implements OnInit {
       } else {
       this.singleBlog = blog;
       this.singleBlog.htmlBody = this.sanitizer.bypassSecurityTrustHtml(this.singleBlog.htmlBody);
+      this.weiboUrl = this.sanitizer.bypassSecurityTrustUrl(`http://service.weibo.com/share/share.php?title=${this.singleBlog.title}&url=${this.currentUrl}`);
       }
-    })
+    });
+
+    this.currentUrl = window.location.href;
+    this.encodedUrl = encodeURIComponent(this.currentUrl);
 
   }
 
@@ -81,5 +112,13 @@ export class DisplaySingleBlogComponent implements OnInit {
       this.auth.isAuthenticated(this.router.url);
     };
   }
+
+  loadDynmicallyScript() {
+    var script = document.createElement('script');
+    script.src = 'https://platform-api.sharethis.com/js/sharethis.js#property=5eb01404b892d40012e344a0&product=inline-share-buttons';
+    script.async =false;
+    document.head.appendChild(script);
+    script.onload = () => this.displayShare = true;
+ }
 
 }
